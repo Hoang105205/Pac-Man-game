a5b7ca2cf444b3f3c9fe9a0cb91e323e734a6fef
QuangNg <qag2702@gmail.com>
2025-03-01 23:30:17 +0700

    Add new file

--- Source/Object/Ghost.py
+++ Source/Object/Ghost.py
@@ -1,3 +1,22 @@
 import pygame
+from constants import SPRITERATIO, SQUARE
 
+class Ghost:
+    def __init__(self, x, y, FileImage):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(FileImage).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (SQUARE * SPRITERATIO, SQUARE * SPRITERATIO))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
 
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        self.rect.topleft = (self.x, self.y)
+
+    def collide(self, rect):
+        return self.rect.colliderect(rect)
